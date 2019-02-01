9dbd2e166936f829448d21caff7ef0fcdc8dccb3
Saulius Lukse <saulius.lukse@gmail.com>
2019-02-01 08:24:38 +0200

    Create show_new_port.py

---
+++ show_new_port.py
@@ -0,0 +1,28 @@
+import sys, re
+import subprocess
+import time
+import serial.tools.list_ports
+
+def grep(regexp):
+    for port, desc, hwid in serial.tools.list_ports.comports():
+        if re.search(regexp, port, re.I) or re.search(regexp, desc) or re.search(regexp, hwid):
+            yield port, desc, hwid
+
+# Discover all COM ports and show them
+port_list_initial = serial.tools.list_ports.comports()
+for port, desc, hwid in port_list_initial:
+    print('-', port, desc)
+
+print('\nWaiting for new USB to SERIAL device to be plugged in...\n')
+
+# Wait for new device to be plugged in and show it
+while True:
+    port_list_poll = serial.tools.list_ports.comports()
+
+    for p in port_list_poll:
+        if p not in port_list_initial:
+            print('-', p)
+            input('\nAll done, press enter to quit')
+            sys.exit(1)
+        else:
+            time.sleep(0.5) # Don't poll too often 
